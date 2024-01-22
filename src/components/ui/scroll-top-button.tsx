8e3256068adfea01c5fffaa1a3984fc8016d0c7d
Felipe Freitas <andrefigueira93@gmail.com>
2024-01-21 23:54:00 -0300

    feat: scroll to top animation

--- src/components/ui/scroll-top-button.tsx
+++ src/components/ui/scroll-top-button.tsx
@@ -1,6 +1,6 @@
 import { ChevronUpIcon } from "@radix-ui/react-icons";
+import { AnimatePresence, motion } from "framer-motion";
 import React, { useCallback, useEffect, useState } from "react";
-import { Button } from "./button";
 
 const ScrollTopButton: React.FC = () => {
   const scrollToTop = useCallback(() => {
@@ -27,14 +27,19 @@ const ScrollTopButton: React.FC = () => {
   }, [showButton]);
 
   return (
-    <Button
-      onClick={scrollToTop}
-      className={`${
-        showButton ? "opacity-100" : "opacity-0"
-      } transition fixed bottom-5 right-5 p-0 rounded-full h-16 w-16 shadow-md`}
-    >
-      <ChevronUpIcon className="text-white animate-bounce h-12 w-12" />
-    </Button>
+    <AnimatePresence>
+      {showButton && (
+        <motion.button
+          onClick={scrollToTop}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          className="transition bg-blue-600 fixed bottom-5 right-5 p-0 rounded-full h-16 w-16 shadow-md z-50"
+        >
+          <ChevronUpIcon className="text-white animate-bounce h-12 w-12 mx-auto" />
+        </motion.button>
+      )}
+    </AnimatePresence>
   );
 };
 
